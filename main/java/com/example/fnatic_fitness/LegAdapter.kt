66d9daef8588444703c94274bae764428a1aadfb
Nopelpunya <147795801+Nopelpunya@users.noreply.github.com>
2024-01-03 22:42:56 +0700

    Add files via upload

---
+++ main/java/com/example/fnatic_fitness/LegAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.fnatic_fitness
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class LegAdapter(private val mList: List<Model>) : RecyclerView.Adapter<LegAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LegAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder (holder: ViewHolder, position : Int){
+        val dataPush = mList[position]
+        holder.setupView(dataPush.Title, dataPush.Title_Description, dataPush.Image)
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    class ViewHolder (view : View) : RecyclerView.ViewHolder(view) {
+        var TitlePushProgram : TextView
+        var DescriptionPushProgram : TextView
+        var ImagePushProgram : ImageView
+
+        init {
+            TitlePushProgram= view.findViewById(R.id.Judul_program)
+            DescriptionPushProgram = view.findViewById(R.id.keterangan_program)
+            ImagePushProgram = view.findViewById(R.id.imageprogram)
+        }
+
+        fun setupView(titleData: String, descriptionData: String, imageData : Int) {
+            TitlePushProgram.text = titleData
+            DescriptionPushProgram.text = descriptionData
+            ImagePushProgram.setImageResource(imageData)
+        }
+    }
+}
